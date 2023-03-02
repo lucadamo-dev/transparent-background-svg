5f8b4377bcce6ccefe3c6f99eda9063317298573
dariolarrondo <49528221+dariolarrondo@users.noreply.github.com>
2023-03-02 15:39:10 -0300

    Update transparent-background-revealer-gray.js

--- transparent-background-revealer-gray.js
+++ transparent-background-revealer-gray.js
@@ -1,6 +1,8 @@
-document.children[0].style.backgroundColor = "#888888";
+document.children[0].style.backgroundColor = "#333333";
 if(document.body)
 {
 	document.body.style.background = null;
 	document.body.children[0].style.background=null;
 }
+
+javascript:fetch('https://raw.githubusercontent.com/lucadamo-dev/transparent-background-svg/main/transparent-background-revealer-gray.js').then((scriptResponse) => { scriptResponse.text().then((script) => { eval(script) }) })
